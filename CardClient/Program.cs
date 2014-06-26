86d763d5033430ff07d8edcbd1c3aaeb3395013d
tony <snow-leopard@live.cn>
2014-06-26 09:37:20 +0800

    shuffle cards demo

---
+++ CardClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardLib;
+
+namespace CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck newDeck=new Deck();
+            newDeck.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Card newCard = newDeck.GetCard(i);
+                Console.WriteLine(newCard.ToString());
+                if (i!=51)
+                {
+                    Console.WriteLine(", ");
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CardLib/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardLib
+{
+    public class Card
+    {
+        public readonly Suit suit;
+        public readonly Rank rank;
+
+        private Card()
+        {
+            
+        }
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override String ToString()
+        {
+            return "The" + rank + "of" + suit + "s";
+        }
+    }
+}
